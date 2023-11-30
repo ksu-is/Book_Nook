bc47c4e904ab1760325fd740623aa4f63f7884f1
VanessaR1 <142240773+VanessaR1@users.noreply.github.com>
2023-11-29 20:57:41 -0500

    Update main.py
    Program can now read csv file named books and will be able to store data. Previously we could only store data in the program while the program was running.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #This is the code I will be adding and editing as I work on my project
 #As of right now I am running and testing most of my code on Jupyter
+#Update: I have transferred to my work from Jupyter to Visual Studio Code and started using csv files
 
-reading_list = []
+
 menu_prompt = """Welcome to Book_Nook! Please enter one of the following options:
 
 - 'a' to add a book
 - 'l' to list the books
+- 's' to search for a book
 - 'q' to quit
 
 What would you like to do? """
@@ -18,25 +20,67 @@ def add_book():
     author = input("Author: ").strip().title()
     year = input("Year of publication: ").strip()
 
-    #dictionary
-    new_book = {
-        "title": title,
-        "author": author,
-        "year": year
-    }
+    #open in append mode
+    with open("book.csv", "a") as reading_list:
+        reading_list.write(f"{title},{author},{year}\n")
     
-    reading_list.append(new_book)
+#find books in database 
+def get_all_books():
+    books = []
+
+    with open("books.csv","r") as reading_list:
+        for book in reading_list:
+            title, author, year = book.strip().split(",")
+            #create dictionary with information and put into books variable
+            books.append({
+                "title": title,
+                "author": author,
+                "year": year
+            })
+    return books
     
-def show_books():
-    for book in reading_list:
+def show_books(books):
+    print()
+    
+    for book in books:
+        title, author, year = book.values()
         print(f"{book['title']}, by {book['author']} ({book['year']})")
 
+    
+    print()
 
+def find_books():
+    reading_list = get_all_books()
+    matching_books = []
+
+    search_term = input("Enter a book title to search for: ").strip().lower()
+
+    for book in reading_list:
+        if search_term in book["title"].lower():
+            matching_books.append(book)
+
+    return matching_books
+    
 while selected_option != "q":
     if selected_option == "a":
         add_book()
     elif selected_option == "l":
-        show_book()
+        reading_list = get_all_books()
+        if reading_list:
+            show_books(reading_list)
+        else:
+            print("Your reading list is empty.")
+        #printing message if reading list is empty rather than just appearing blank. 
+    elif selected_option == "s":
+        matching_books = find_books()
+
+        if matching_books:
+            show_books(matching_books)
+        else:("Sorry, no books found matching that search term.")
+    else:
+        print(f"Sorry, '{selected_option}' isn't a valid option.")
+        
+    selected_option = input(menu_prompt).strip().lower()
     else:
         print(f"Sorry, '{selected_option}' isn't a valid option.")
         
